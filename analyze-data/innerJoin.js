65df0cfb653d14487970c613265e35ae465c3314
elishakay <kramer1346@gmail.com>
2018-12-03 17:46:03 +0200

    succesfully logging an object that has been inner joined with referring domains and articles

--- analyze-data/innerJoin.js
+++ analyze-data/innerJoin.js
@@ -1,3 +1,29 @@
 const allBacklinks = require('../output/allBacklinks');
+const refDomains = require('../output/refDomains');
+const newArr = [];
 
-console.log('allBacklinks:', allBacklinks);
+allBacklinks.forEach(function (backlinkRecord) {
+    let sourceUrl = backlinkRecord['sourceUrl'];
+
+    refDomains.forEach(function (refDomainRecord) {
+    	let domain = refDomainRecord['domain'];
+
+    	if(sourceUrl.includes(domain)){
+			let domainTrustScore = refDomainRecord['domainTrustScore'];
+    		let pageTrustScore = backlinkRecord['pageTrustScore'];
+    		let sourceTitle = backlinkRecord['sourceTitle'];
+
+    		newArr.push(
+    			{sourceUrl, 
+    			 domain,
+    			 domainTrustScore,
+    			 pageTrustScore,
+    			 sourceTitle
+    			}
+    		)
+    	}
+    });
+    
+});
+
+console.log('newArr.length(): ', newArr.length);
