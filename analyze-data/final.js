3641cc8c5c819db7574c27b61e0c9b6a3ad42fd3
elishakay <kramer1346@gmail.com>
2018-12-05 14:19:23 +0200

    format for creating csv from array of objects

--- analyze-data/final.js
+++ analyze-data/final.js
@@ -1,5 +1,7 @@
 let fs = require('fs');
 let alasql = require('alasql');
+const { convertArrayToCSV } = require('convert-array-to-csv');
+const converter = require('convert-array-to-csv');
 
 const backlinksAndDomains = require('../output/backlinksAndDomains.js');
 const ftlResults = require('../output/ftlResults');
@@ -15,20 +17,21 @@ function compare(a,b) {
 let newArr = alasql('SELECT * \
             FROM ? arrA JOIN ? arrB USING domain', [backlinksAndDomains, ftlResults]);
 
-// source: https://stackoverflow.com/questions/39885893/how-to-find-duplicate-values-in-a-javascript-array-of-objects-and-output-only-u
+// bring records with names to the top
 newArr.sort(compare);
 
+// remove duplicate email records
+// source: https://stackoverflow.com/questions/39885893/how-to-find-duplicate-values-in-a-javascript-array-of-objects-and-output-only-u
 newArr = newArr.filter((set => f => !set.has(f.email) && set.add(f.email))(new Set));
 
-
+//create csv File
+newArr = convertArrayToCSV(newArr);
 
 console.log('newArr: ', newArr);
 
 console.log('newArr.length(): ', newArr.length);
 
-
-
-// fs.writeFile('../output/backlinksDomainsAndEmails.js', JSON.stringify(newArr), function(err, newArr){
-//             if (err) console.log(err);
-//             console.log("Successfully Written to File.");
-// });
+fs.writeFile('../output/final.csv', JSON.stringify(newArr), function(err, newArr){
+            if (err) console.log(err);
+            console.log("Successfully Written to File.");
+});
